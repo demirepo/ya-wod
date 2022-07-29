0259cbe2bd195993ceb535ecd67c826f83929542
Evgeny Petrunin <duen@mail.ru>
2022-07-29 17:27:34 +0300

    v0.1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { schedule } from 'node-cron';
 import { getWods } from './services/getWods.js';
 import pgdb from './services/postgres.js';
+import { cronjob as CRON_JOB} from './constants/cron.js';
 
 //------------------------------------------------------------------ launch
 
@@ -12,8 +13,9 @@ const wods2db = async () => {
       'INSERT INTO wods(src,dst,dst_extended,date) values($1,$2,$3,$4)',
       [wod.wod, wod.tr, wod.tr2, wod.date]
     );
+    console.log('Добавлено в БД: ', wod.wod);
   }
 };
 
-schedule('0 8 * * *', wods2db);
-console.log('Отслеживание word of the day Яндекс-переводчика начато');
+schedule(CRON_JOB, wods2db);
+console.log('Отслеживание word of the day Яндекс-переводчика начато с расписанием cron: ', CRON_JOB);
