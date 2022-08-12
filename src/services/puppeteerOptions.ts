0cfe93c5e5285032a704949fbbceba0e44b8ee20
Evgeny Petrunin <duen@mail.ru>
2022-08-12 20:25:40 +0300

    cross platform improvements

---
+++ src/services/puppeteerOptions.ts
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+
+if (process.platform === 'win32') dotenv.config();
+if (process.platform === 'linux') dotenv.config({ path: '../../.env' });
+
+const winExecPath = 'C:\\Program Files\\chrome-win\\chrome.exe';
+const linuxExecPath = '/snap/bin/chromium';
+const withHead = {
+  headless: false,
+  args: ['--start-maximized', '--window-size=1920x1080'],
+};
+
+let puppeteerOptions = {
+  ignoreHTTPSErrors: true,
+};
+
+switch (process.platform) {
+  case 'win32':
+    puppeteerOptions = {
+      ...puppeteerOptions,
+      ...{ executablePath: winExecPath },
+    };
+
+    if (process.env.PUP_VISIBLE) {
+      console.log('выполняется слияние');
+
+      puppeteerOptions = {
+        ...puppeteerOptions,
+        ...withHead,
+      };
+    }
+    break;
+
+  case 'linux':
+    puppeteerOptions = {
+      ...puppeteerOptions,
+      ...{ executablePath: linuxExecPath },
+    };
+    break;
+
+  default:
+    break;
+}
+
+console.log('puppeteerOptions:', puppeteerOptions);
+
+export default puppeteerOptions;
